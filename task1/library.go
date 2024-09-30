fcf8fd786fa708d799e72315c47637140fd1753f
Timur Nasretdinov <timurka_nasretdinov@mail.ru>
2024-09-30 22:31:33 +0300

    Save book id when adding

--- task1/library.go
+++ task1/library.go
@@ -2,17 +2,22 @@ package main
 
 type Library struct {
 	storage     Storage
+	books_id    map[string]int
 	id_function func(string) int
 }
 
 func (library *Library) addBook(book Book) {
 	var book_id = library.id_function(book.name)
+	library.books_id[book.name] = book_id
 	library.storage.addBook(book_id, book)
 }
 
 func (library *Library) getBook(name string) (Book, bool) {
-	var book_id = library.id_function(name)
-	return library.storage.getBook(book_id)
+	var val, ok = library.books_id[name]
+	if ok {
+		return library.storage.getBook(val)
+	}
+	return Book{}, false
 }
 
 func (library *Library) changeIdFunction(id_function func(string) int) {
