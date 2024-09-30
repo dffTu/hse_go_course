29525110dbb1730b936e543e46e3042bbda8f1e0
Timur Nasretdinov <timurka_nasretdinov@mail.ru>
2024-09-30 21:31:01 +0300

    Added Storage struct

---
+++ task1/storage.go
@@ -0,0 +1,17 @@
+package main
+
+type Storage struct {
+	books map[int]Book
+}
+
+func (storage *Storage) addBook(id int, book Book) {
+	(*storage).books[id] = book
+}
+
+func (storage *Storage) getBook(id int) (Book, bool) {
+	book, ok := (*storage).books[id]
+	if ok {
+		delete((*storage).books, id)
+	}
+	return book, ok
+}
