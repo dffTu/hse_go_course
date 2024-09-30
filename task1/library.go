57beab1d21108f19ec78e0aa363e74b4445b024e
Timur Nasretdinov <timurka_nasretdinov@mail.ru>
2024-09-30 21:46:32 +0300

    Added functions to change id_function and storage in Library

--- task1/library.go
+++ task1/library.go
@@ -14,3 +14,11 @@ func (library *Library) getBook(name string) (Book, bool) {
 	var book_id = library.id_function(name)
 	return library.storage.getBook(book_id)
 }
+
+func (library *Library) changeIdFunction(id_function func(string) int) {
+	library.id_function = id_function
+}
+
+func (library *Library) changeStorage(storage Storage) {
+	library.storage = storage
+}
