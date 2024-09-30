8297ba4126650df7eb06d8befb6ba856483ba8ff
Timur Nasretdinov <timurka_nasretdinov@mail.ru>
2024-09-30 21:36:10 +0300

    Added Library struct

---
+++ task1/library.go
@@ -0,0 +1,16 @@
+package main
+
+type Library struct {
+	storage     Storage
+	id_function func(string) int
+}
+
+func (library *Library) addBook(book Book) {
+	var book_id = library.id_function(book.name)
+	library.storage.addBook(book_id, book)
+}
+
+func (library *Library) getBook(name string) (Book, bool) {
+	var book_id = library.id_function(name)
+	return library.storage.getBook(book_id)
+}
