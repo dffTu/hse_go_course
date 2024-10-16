f5b6c1fc7a64fd523feeb7c06b4fe2c5dbe4cf23
Timur Nasretdinov <timurka_nasretdinov@mail.ru>
2024-10-16 17:37:00 +0300

    Added graceful shutdown

--- task2/server/server/server.go
+++ task2/server/server/server.go
@@ -1,9 +1,15 @@
 package server
 
 import (
+	"context"
 	"fmt"
 	"net/http"
+	"os"
+	"os/signal"
+	"syscall"
 	"task2/server/request"
+
+	"golang.org/x/sync/errgroup"
 )
 
 type Server struct {
@@ -24,11 +30,31 @@ func (server *Server) Start() {
 	for _, val := range server.requests {
 		server.mux.HandleFunc(val.Path, val.Handler)
 	}
+
+	ctx, cancel := context.WithCancel(context.Background())
+
+	go func() {
+		c := make(chan os.Signal, 1)
+		signal.Notify(c, os.Interrupt, syscall.SIGTERM, syscall.SIGINT)
+
+		<-c
+		cancel()
+	}()
+
 	httpServer := http.Server{
 		Addr:    server.address,
 		Handler: server.mux,
 	}
-	if err := httpServer.ListenAndServe(); err != nil {
-		fmt.Println(err)
+
+	g, gCtx := errgroup.WithContext(ctx)
+	g.Go(func() error {
+		return httpServer.ListenAndServe()
+	})
+	g.Go(func() error {
+		<-gCtx.Done()
+		return httpServer.Shutdown(context.Background())
+	})
+	if err := g.Wait(); err != nil {
+		fmt.Printf("exit reason: %s \n", err)
 	}
 }
